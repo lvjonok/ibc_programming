9cb59e9230613faea6b229b321d003ddc814f503
lvjonok <kozlov.l.a@mail.ru>
2020-08-13 15:47:20 +0300

    added implementation of permutations generator

--- fourth_day/task2.cpp
+++ fourth_day/task2.cpp
@@ -9,6 +9,28 @@ void print_path(vector<int> path){
     }
     cout << endl;
 }
+vector<int> get_permutations(vector<int> array, vector<int> left_values){
+    vector<vector<int>> answer;
+    if (left_values.size() != 0){
+        for (auto value: left_values){
+            vector<int> new_array = array;
+            new_array.push_back(value);
+            vector<int> new_values;
+            for (auto left_value: left_values){
+                if (left_value != value) new_values.push_back(left_value);
+            }
+            vector<int> local_ans = get_permutations(new_array, new_values);
+            answer.push_back(local_ans);
+        }
+        return array;
+    } else {
+        print_path(array);
+        return array;
+    }
+}
+
+
+
 int main(){
     int n;
     cin >> n;
@@ -18,10 +40,10 @@ int main(){
         cin >> numbers[i];
     }
     auto t1 = chrono::high_resolution_clock::now();
-    do {
-        print_path(numbers);
-    }  while (next_permutation(numbers.begin(), numbers.end()));
-
+    // do {
+    //     print_path(numbers);
+    // }  while (next_permutation(numbers.begin(), numbers.end()));
+    get_permutations(vector<int>(), numbers);
     auto t2 = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
     cout << "Time is " << duration << endl; // 218540999 nanosecs for 7 numbers
