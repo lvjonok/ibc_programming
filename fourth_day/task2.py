00ed76912bf25ffb4b66ca4619dfaddb3ee48a59
lvjonok <kozlov.l.a@mail.ru>
2020-08-13 15:07:15 +0300

    added my implementation of permutations

--- fourth_day/task2.py
+++ fourth_day/task2.py
@@ -1,10 +1,27 @@
 import time, itertools
 
+perm_answer = []
+
+def get_permutations(array, left_values):
+    answer = []
+    if left_values:
+        for new_value in left_values:
+            new_array = array + [new_value]
+            new_left = left_values[:]
+            new_left.remove(new_value)
+            answer.append(get_permutations(new_array, new_left))
+        return answer
+    else:
+        global perm_answer
+        perm_answer.append(array)
+        return array
+
 numbers = input().split()
 
 t1 = time.time()
 
-for perm in itertools.permutations(numbers, len(numbers)):
+get_permutations([], numbers)
+for perm in perm_answer:
     print(" ".join(perm))
 
 t2 = time.time()
